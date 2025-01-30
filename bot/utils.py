08ad9cdda7d27081ce3b2d4e1f0b334f0d6dcbde
Dzubei <chikita@sb.by>
2025-01-30 10:32:19 +0300

    create uv

--- bot/utils.py
+++
@@ -1,9 +0,0 @@
-import telebot
-from bot.config import API_TOKEN  # Укажите ваш API_TOKEN
-
-# Инициализация бота
-bot = telebot.TeleBot(API_TOKEN)
-
-def send_notification(message: str, chat_id: str):
-    """Отправка уведомления в Telegram"""
-    bot.send_message(chat_id, message)
