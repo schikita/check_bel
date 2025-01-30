49b06d6634fbd723e3872b72a7f32f84273b1b97
nik <mkisel28@gmail.com>
2025-01-30 15:17:39 +0300

    del print

--- src/detect_lang.py
+++ src/detect_lang.py
@@ -10,7 +10,6 @@ model_custom = fasttext.load_model(str(MODEL_PATH))
 def detect_language(text: str) -> Literal["rus", "bel"]:
     """Определяет язык текста (русский или белорусский) с помощью обученной модели."""
     label, _ = model_custom.predict(text)
-    print(label)
     return 'rus' if label[0] == '__label__ru' else 'bel'
 
 print(detect_language("Привет, как дела?")) 
